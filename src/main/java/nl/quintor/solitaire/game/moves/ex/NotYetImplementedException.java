784680892ca68e156bef46054c615d6aea54a02b
Tim Damen <tim.damen80@gmail.com>
2020-10-05 16:19:45 +0200

    delete files

--- src/main/java/nl/quintor/solitaire/game/moves/ex/NotYetImplementedException.java
+++
@@ -1,7 +0,0 @@
-package nl.quintor.solitaire.game.moves.ex;
-
-public class NotYetImplementedException extends MoveException {
-    public NotYetImplementedException(String message) {
-        super(message);
-    }
-}
